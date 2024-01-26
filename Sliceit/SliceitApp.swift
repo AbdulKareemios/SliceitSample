42c19e56018f9b7b0d337aed39a365ec8d09d920
Abdul <abdul.kareem@vimmerse.net>
2024-01-27 01:17:51 +0500

    Initial Commit

---
+++ Sliceit/SliceitApp.swift
@@ -0,0 +1,17 @@
+//
+//  SliceitApp.swift
+//  Sliceit
+//
+//  Created by AK on 1/27/24.
+//
+
+import SwiftUI
+
+@main
+struct SliceitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
